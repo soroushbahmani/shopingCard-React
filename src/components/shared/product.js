2b0da7a07fce7cf80d1f00bfcd003f5792e8574b
soroushbahmani <soroush.bahmani.ir@gmail.com>
2022-04-25 19:50:43 +0430

    write html base product.js

---
+++ src/components/shared/product.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function product() {
+    return (
+        <div>
+            <img src='' alt='img' />
+            <h3>title</h3>
+            <p>
+                price
+            </p>
+            <div>
+
+                <a href=''>detaled</a>
+            </div>
+            <div>
+                <button>add to card</button>
+            </div>
+        </div>
+    )
+}
+
+export default product
