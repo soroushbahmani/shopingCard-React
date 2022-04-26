b569f968f06c6759b8fe6c5983ac1d1a8179ca24
soroushbahmani <soroush.bahmani.ir@gmail.com>
2022-04-26 15:14:17 +0430

    create Link in product  to ridirect pageDitails

--- src/components/shared/product.js
+++ src/components/shared/product.js
@@ -1,19 +1,20 @@
 import React from 'react'
-
+//route
+import { Route, Link } from 'react-router-dom'
 //helpers
-import {Shorten} from '../../helpers/function'
+import { Shorten } from '../../helpers/function'
 
-function Product({ProductData}) {
+function Product({ ProductData }) {
     return (
-        <div style={{margin:' 0 20px'}}>
-            <img src={ProductData.image} alt='img' style={{"width" : '200px'}} />
+        <div style={{ margin: ' 0 20px' }}>
+            <img src={ProductData.image} alt='img' style={{ "width": '200px' }} />
             <h3>{Shorten(ProductData.title)}</h3>
             <p>
                 {ProductData.price}
             </p>
             <div>
 
-                <a href=''>detaled</a>
+                <Link to={`/product/${ProductData.id}`}>detaled</Link>
             </div>
             <div>
                 <button>add to card</button>
