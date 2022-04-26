850748a2ffb2fd76bb7bd369aebae8048e1dc0d8
soroushbahmani <soroush.bahmani.ir@gmail.com>
2022-04-26 15:45:14 +0430

    fetch data with props in productDetails

--- src/components/ProductDetailes.js
+++ src/components/ProductDetailes.js
@@ -1,11 +1,18 @@
-import React from 'react'
-import {useParams}  from 'react-router-dom'
+import React, { useContext } from 'react'
+// context
+import { ProductContext } from '../context/ProductContextProvider'
+// route
+import { useParams } from 'react-router-dom'
 
-const ProductDetailes = ()=> {
+const ProductDetailes = () => {
     const props = useParams()
-  return (
-    <div>ProductDetailes</div>
-  )
+    const data = useContext(ProductContext);
+    const product = data[props.id - 1]
+    const { image, description, title, price, category } = data
+
+    return (
+        <div>ProductDetailes</div>
+    )
 }
 
 export default ProductDetailes
