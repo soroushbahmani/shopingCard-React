0b6d9ae44d02ffbd35f3122114fcb40c0dcc08aa
soroushbahmani <soroush.bahmani.ir@gmail.com>
2022-04-26 20:36:06 +0430

    write func auntity in helpers

--- src/helpers/function.js
+++ src/helpers/function.js
@@ -9,4 +9,13 @@ const isInCard = (state, id) => {
     return result;
 }
 
-export { Shorten,isInCard }
+const quantity = (state, id) => {
+    const index = state.selectedItems.findIndex(item => item.id === id)
+    if (index === -1) {
+        return false
+    } else {
+        return state.selectedItems[index].quantity;
+    }
+}
+
+export { Shorten, isInCard, quantity }
